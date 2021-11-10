87e078547bb5a386f705dd31a36683f39516ed41
cti11man4 <cti11man211@gmail.com>
2021-11-09 21:08:41 -0800

    Dice roller implementation

--- commands/roll.js
+++ commands/roll.js
@@ -3,8 +3,93 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('roll')
-		.setDescription('Rolls some dice.'),
+		.setDescription('Rolls some dice. Defaults to d20.')
+		.addStringOption(option =>
+			option.setName('dice')
+				.setDescription('Uses standard dice notation.')
+				.setRequired(false))
+		.addStringOption(option =>
+			option.setName('advantage')
+				.setDescription('Roll with advantage or disadvantage.')
+				.setRequired(false)
+				.addChoice('Advantage', 'adv')
+				.addChoice('Disadvantage', 'dis')),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		let dice = interaction.options.getString('dice');		// get dice value (if any)
+		const adv = interaction.options.getString('advantage');	// get advantage or disadvantage (if any)
+
+		if(dice == null) dice = 'd20';							// check if dice value is null
+		else dice = dice.toLowerCase().replaceAll(' ','');		// remove whitespace
+
+		// regex: ensures dice value is of the form
+		// #d# (+/- #(d#) +/- #(d#) +/- ...)
+		const re = /\d*d\d+(((\+|\-)\d*d\d+)*((\+|\-)\d*)*)*/;
+		const found = dice.match(re);							// check if dice value matches regex
+		if(found == null) {
+			await interaction.reply('Sorry, I can\'t roll that input.');
+			return;
+		}
+
+		// splits rolls and constants into an array, preserves sign
+		let rolls = [dice.match(/\d*d\d+/i)[0]];
+		console.log(dice.match(/\d*d\d+/i)[0]);
+		for(r of dice.matchAll(/((\+|\-)\d*d\d+)|((\+|\-)\d*)/g)) {
+			if(r[0] != '') rolls.push(r[0]);
+		}
+
+		let out = 'Here\'s what I rolled:\n'					// output string
+		let pos = true;											// indicates positive or negative roll
+		let curr = 0;											// current roll
+		let total = 0;											// current roll total
+		let result = 0;											// total result
+		let constant = 0;										// result of constants
+
+		// organizes and calculates rolls
+		for(r of rolls) {
+			// calculate constants separately
+			if(!(r.includes('d'))) {
+				constant += parseInt(r);
+				continue;
+			}
+
+			// checks and removes sign
+			if(r.search(/\+|\-/) > -1) {
+				if(r[0] == '-') pos = false;
+				else pos = true;
+				r = r.substring(1);
+			}
+
+			// simplifies dice
+			if(r[0] == 'd') r = '1'.concat(r);
+
+			out += '`' + r + ': ';
+			curr = 0;
+			total = 0;
+
+			// generate rolls
+			for(let i = 0; i < parseInt(r.substring(0,parseInt(r.indexOf('d')))); i++) {
+				curr = Math.floor(Math.random() * parseInt(r.substring(1 + parseInt(r.indexOf('d'))))) + 1;
+				total += curr;
+				if(i > 0) {
+					out += ', ';
+				}
+				out += curr;
+			}
+
+			if(!(r[0] == '1' && r[1] == 'd')) out += ' => ' + total;
+			out += '`\n';
+
+			if(pos) result += total;
+			else result -= total;
+		}
+
+		if(constant != 0) {
+			out += '`Constant: ' + constant + '`\n';
+			result += constant;
+		}
+		out += '`' + dice + ' => ' + result +'`';
+
+		console.log(out);
+		await interaction.reply(out);
 	},
 };
--- index.js
+++ index.js
@@ -4,16 +4,7 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
+// collect the list of commands
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -22,5 +13,26 @@ for (const file of commandFiles) {
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
+
+// prints once when bot is online
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// command handler
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return; // returns on non-slash commands
+
+	const command = client.commands.get(interaction.commandName); // get name of command
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction); // calls execute function for given command
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 client.login(token);
